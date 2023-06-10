d2c8761d89a200a61cc21845aa517163dd447153
alvarosabu <alvaro.saburido@gmail.com>
2023-06-10 17:24:15 +0200

    feat: dark mode toggle

---
+++ src/components/as-dark-mode-toggle/AsDarkModeToggle.story.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import AsDarkModeToggle from './AsDarkModeToggle.vue'
+</script>
+
+<template>
+  <Story>
+    <Variant title="playground">
+      <template #default>
+        <div p-4>
+          <AsDarkModeToggle />
+        </div>
+      </template>
+    </Variant>
+  </Story>
+</template>
