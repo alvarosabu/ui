008cc16d54b084893052bae18500f119f8bc4bf3
alvarosabu <alvaro.saburido@gmail.com>
2023-06-10 16:33:14 +0200

    fix: text image direction to row

---
+++ src/components/as-badge/index.ts
@@ -0,0 +1,1 @@
+export * from './AsBadge.vue'
--- src/components/as-text-image/AsTextImage.vue
+++ src/components/as-text-image/AsTextImage.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
 import { computed } from 'vue'
-import { textImgDirection } from './const'
 import AsImg from '../as-img/AsImg.vue'
 import AsGraphic from '../as-graphic/AsGraphic.vue'
+import { textImgDirection } from './const'
 
-export type TextImageProps = {
+export interface TextImageProps {
   media: {
     src: string
     alt: string
@@ -14,11 +14,12 @@ export type TextImageProps = {
 }
 const {
   media,
-  direction = textImgDirection.DEFAULT,
+  direction = textImgDirection.ROW,
 } = defineProps<TextImageProps>()
 
 const isReverse = computed(() => direction === textImgDirection.REVERSE)
 </script>
+
 <template>
   <div
     class="flex"
@@ -29,12 +30,26 @@ const isReverse = computed(() => direction === textImgDirection.REVERSE)
     "
   >
     <div class="relative w-full sm:w-1/2 py-4 min-h-300px sm:min-h-375px">
-      <AsGraphic class="absolute top-16 scale-75" :class="isReverse ? 'right-0' : 'left-0'" />
-      <div class="absolute w-2/3 rounded-xl" :class="isReverse ? 'left-0' : 'right-0'">
-        <AsImg class="shadow-lg" :src="media.src" :alt="media.alt" :aspect-ratio="media.aspectRatio" />
+      <AsGraphic
+        class="absolute top-16 scale-75"
+        :class="isReverse ? 'right-0' : 'left-0'"
+      />
+      <div
+        class="absolute w-2/3 rounded-xl"
+        :class="isReverse ? 'left-0' : 'right-0'"
+      >
+        <AsImg
+          class="shadow-lg"
+          :src="media.src"
+          :alt="media.alt"
+          :aspect-ratio="media.aspectRatio"
+        />
       </div>
     </div>
-    <div class="w-full sm:w-1/2 prose" :class="isReverse ? 'sm:pr-16' : 'sm:pl-16'">
+    <div
+      class="w-full sm:w-1/2 prose"
+      :class="isReverse ? 'sm:pr-16' : 'sm:pl-16'"
+    >
       <slot />
     </div>
   </div>
