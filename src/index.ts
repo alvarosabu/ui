9104f5e3c754369cc58c6dfc7fe7960042249186
alvarosabu <alvaro.saburido@gmail.com>
2023-06-11 22:28:16 +0200

    feat: export unocss config

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
+import { ASRules } from './styles/rules'
+import { ASTypographyOptions } from './styles/typography'
+import { ASShortcuts } from './styles/shortcuts'
+import { ASIconsOptions } from './styles/icons'
+import { ASWebFontsOptions } from './styles/fonts'
 import 'uno.css'
 import '@unocss/reset/tailwind.css'
+import { ASTheme } from './styles/as-theme'
 
 export * from './components'
 export * from './composables'
+
+export default {
+  theme: ASTheme,
+  fonts: ASWebFontsOptions,
+  icons: ASIconsOptions,
+  shortcuts: ASShortcuts,
+  typography: ASTypographyOptions,
+  rules: ASRules,
+}
